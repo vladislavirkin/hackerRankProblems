a490f00027225aafad5fca2ca92bcfc1b3ef2b60
Vladislav Irkin <kakoy-to-sportsman@outlook.com>
2022-03-31 16:04:02 +0300

    ➕ feat(app): add new soutions for day 2

---
+++ src/HackerRankSolutions/ProblemSolving/CountingSort.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace HackerRankSolutions.ProblemSolving;
+
+[TestFixture]
+public class CountingSort
+{
+    public static IEnumerable<TestCaseData> TestSource()
+    {
+        yield return new TestCaseData(new List<int> { 0, 1, 2, 1, 2, 3 }, new List<int> { 1, 2, 2, 1 });
+    }
+
+    [Test]
+    [TestCaseSource(nameof(TestSource))]
+    public void Solve(List<int> items, List<int> frequencyList)
+    {
+        var ret = Enumerable.Repeat(0, items.Max() + 1).ToList();
+
+        foreach (var t in items)
+            ret[t]++;
+
+        Assert.AreEqual(frequencyList, ret, "Frequency list calculated incorrectly.");
+    }
+}
---
+++ src/HackerRankSolutions/ProblemSolving/FlippingMatrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace HackerRankSolutions.ProblemSolving;
+
+[TestFixture]
+public class FlippingMatrix
+{
+    public static IEnumerable<TestCaseData> TestSource()
+    {
+        yield return new TestCaseData(new List<List<int>> { new() { 112, 42, 83, 119 }, new() { 56, 125, 56, 49 }, new() { 15, 78, 101, 43 }, new() { 62, 98, 114, 108 } }, 414);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(TestSource))]
+    public void Solve(List<List<int>> matrix, int expectedSum)
+    {
+        var ret = 0;
+
+        var n = matrix.Count / 2;
+        var tmp = new List<int>();
+        
+        for (var row = 0; row < n; row++)
+        {
+            for (var column = 0; column < n; column++)
+            {
+                tmp.Add(matrix[row][2 * n - column - 1]);
+                tmp.Add(matrix[row][column]);
+                tmp.Add(matrix[2 * n - row - 1][column]);
+                tmp.Add(matrix[2 * n - row - 1][2 * n - column - 1]);
+             
+                ret += tmp.Max();
+                tmp.Clear();
+            }
+        }
+
+        Assert.AreEqual(expectedSum, ret, "Sum of the values in the upper-left quadrant calculated incorrectly.");
+    }
+}
