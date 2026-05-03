e8e50531461a8a6f8fa6a939dfd87cdb40c23ead
Vladislav Irkin <vladislavirkin@outlook.com>
2026-05-03 19:54:54 +0200

    add sql solution (175)

---
+++ src/SqlSolutions/LeetCode/N175_CombineTwoTables.sql
@@ -0,0 +1,16 @@
+-- Description = ("Easy" + "https://leetcode.com/problems/combine-two-tables/")
+-- Arrange
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+-- Act
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a ON Person.personId = Address.personId
