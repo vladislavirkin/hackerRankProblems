8d898ba54456a7241890c0248b87ff9855af9b2f
Vladislav Irkin <vladislavirkin@outlook.com>
2026-05-12 10:51:59 +0200

    fix build

--- src/SqlSolutions/LeetCode/N175_CombineTwoTables.sql
+++ src/SqlSolutions/LeetCode/N175_CombineTwoTables.sql
@@ -1,5 +1,7 @@
 -- Description = ("Easy" + "https://leetcode.com/problems/combine-two-tables/")
 -- Arrange
+
+/*
 Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
 Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
 Truncate table Person
@@ -8,6 +10,7 @@ insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
 Truncate table Address
 insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
 insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+*/
 
 -- Act
 
